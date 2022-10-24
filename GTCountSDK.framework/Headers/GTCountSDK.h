ad3d100fc28d3f351111c9d431e8fea436ae6f65
joy <cailj@getui.com>
2022-10-24 10:32:49 +0800

    update: 更新1.3.5.0版本

--- GTCountSDK.framework/Headers/GTCountSDK.h
+++ GTCountSDK.framework/Headers/GTCountSDK.h
@@ -5,7 +5,7 @@
 //  Created by GeTui on 2017/7/18.
 //  Copyright © 2017年 GeTui. All rights reserved.
 //
-//  Version : GSIDO-IOS-1.3.4.0
+//  Version : GSIDO-IOS-1.3.5.0
 
 #import <Foundation/Foundation.h>
 
