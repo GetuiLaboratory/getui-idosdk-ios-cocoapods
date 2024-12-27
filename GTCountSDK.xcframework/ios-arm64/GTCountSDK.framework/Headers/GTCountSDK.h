a00cf69c5945a6e193f344f22636c5e49462ed7b
linwanting <linwt@getui.com>
2024-12-27 10:29:41 +0800

    update: 更新2.0.9.0版本

--- GTCountSDK.xcframework/ios-arm64/GTCountSDK.framework/Headers/GTCountSDK.h
+++ GTCountSDK.xcframework/ios-arm64/GTCountSDK.framework/Headers/GTCountSDK.h
@@ -5,7 +5,7 @@
 //  Created by GeTui on 2017/7/18.
 //  Copyright © 2017年 GeTui. All rights reserved.
 //
-//  Version : GSIDO-IOS-2.0.8.0
+//  Version : GSIDO-IOS-2.0.9.0
 
 #import <Foundation/Foundation.h>
 
